232194dd381fb1f4c7ca9949901bea4183d76b87
Rogeraprendizajem1 <aprendizajesl6@gmail.com>
2024-04-18 11:03:12 -0600

    file1 removed

---
+++ prueba.py
@@ -0,0 +1,8 @@
+#Este es un programa de prueba que imprime el texto "Hola mundo"
+
+Print("Hola mundo y a todo el mundo - roger")
+print("Adios, roger")
+#Este es un programa de prueba que imprime el texto "Hola mundo"
+
+Print("Hola mundo y a todo el mundo - roger")
+Print("Adios, Roger")  
